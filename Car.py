167dc9b5765cb18ed5a66cfba3d5b8756b3f3f9d
JMGPhactory <jmg.phactory@gmail.com>
2020-05-16 00:39:37 +0200

    main extedned, car script enhanced (points for corner collisions - high level stuff :P), variables (speed,rotation,wheelRot,...)

---
+++ Car.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import pygame as PG
+import math
+
+class Car:
+    def __init__(self, path, size):
+        self.orig_image = PG.image.load(path)
+
+        w = self.orig_image.get_rect().size[1]
+        h = self.orig_image.get_rect().size[0]
+        r = w/h
+
+        self.orig_image = PG.transform.scale(self.orig_image, (size,int(size*r)))
+        self.image = self.orig_image.copy()
+
+        self.position = (0,0)
+        self.rotation = 0
+
+        self.speed = 0
+        self.wheelDir = 0
+        self.MAX_wheelDir = 50
+
+    def get_pos(self):
+        return(int(self.position[0]), int(self.position[1]))
+    
+    def get_center_pos(self, pos):
+        return (int(pos[0] - int(self.image.get_width()/2)), int(pos[1] - int(self.image.get_height()/2)))
+
+    def get_corner_points(self):
+        car_height = self.orig_image.get_height()
+        car_width = self.orig_image.get_width()
+
+        angle = 23
+        c = math.sqrt((car_height/2)**2 + (car_width/2)**2) - 6 
+
+        x1 = math.cos(math.radians(self.rotation-angle)) * c
+        y1 = math.sin(math.radians(self.rotation-angle)) * c
+        x2 = math.cos(math.radians(self.rotation+angle)) * c
+        y2 = math.sin(math.radians(self.rotation+angle)) * c
+
+        x3 = math.cos(math.radians(self.rotation+180-angle)) * c
+        y3 = math.sin(math.radians(self.rotation+180-angle)) * c
+
+        x4 = math.cos(math.radians(self.rotation+180+angle)) * c
+        y4 = math.sin(math.radians(self.rotation+180+angle)) * c
+
+        center_pos = self.get_pos()
+        NE = (int(center_pos[0] + x1), int(center_pos[1] - y1))
+        NW = (int(center_pos[0] + x3), int(center_pos[1] - y3))
+        SE = (int(center_pos[0] + x2), int(center_pos[1] - y2))
+        SW = (int(center_pos[0] + x4), int(center_pos[1] - y4))
+
+        return (NW,NE,SW,SE)
+
+
+    def rel_rotate(self, angle):
+        self.rotation += angle
+        self.image = PG.transform.rotate(self.orig_image, self.rotation)
+
+    def abs_rotate(self, angle):
+        self.rotation = angle
+        self.image = PG.transform.rotate(self.orig_image, self.rotation)
+
+    def update_pos(self):
+        self.position = (self.position[0] + (self.speed * math.cos(math.radians(self.rotation))),
+                         self.position[1] + (self.speed * math.sin(math.radians(self.rotation+180))))
+
+    def set_pos(self, pos):
+        self.position = pos
--- main.py
+++ main.py
@@ -5,21 +5,26 @@ import sys
 
 from Boundary import Boundary
 from Boundary import CircleBoundary as CB
+from Car import Car
 
 PG.init()
 screen = PG.display.set_mode((800,800))
 PG.display.set_caption("RACE CAR AI")
-
+clock = PG.time.Clock()
 
 BRUSH_RADIUS = 20
 TRACK_IMAGE = None
 boundaries = []
+
+car = Car("data/carClipArt.jpg", 100)
+car.position = (300,300)
+car.rotation = 0
+
 while True:
     screen.fill((50,50,50))
 
     for b in boundaries:
         b.update()
-
 
     for event in PG.event.get():
         if event.type == PG.QUIT: sys.exit()
@@ -51,10 +56,25 @@ while True:
                 sys.exit()
 
             if event.key == PG.K_KP_ENTER:
-                PG.image.save(screen, "track.png")
-                TRACK_IMAGE = PG.image.load("track.png")
+                PG.image.save(screen, "data/track.png")
+                TRACK_IMAGE = PG.image.load("data/track.png")
 
     aPos = PG.mouse.get_pos()
     PG.draw.circle(screen, PG.Color('white'), aPos, BRUSH_RADIUS, 1)
 
+    car.speed = 5 
+    car.rel_rotate(2)
+    car.update_pos()
+
+    #CENTER POS je potřeba k vykreslování
+    screen.blit(car.image, car.get_center_pos(car.position))
+    #Get_pos je střed auta
+    #PG.draw.circle(screen, PG.Color('white'), car.get_pos(), 5, 0)
+
+    print(car.get_corner_points())
+    for point in car.get_corner_points():
+        PG.draw.circle(screen, PG.Color('white'), point, 5, 0)
+
     PG.display.flip()
+
+    clock.tick(60)
