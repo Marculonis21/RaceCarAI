fe3bf5eb8e6f76a344b88cb712f8b7c79e39443e
Marculonis21 <markbecvar@gmail.com>
2023-12-25 18:10:50 +0100

    GA run tests

--- V2/main.py
+++ V2/main.py
@@ -8,10 +8,14 @@ import numpy as np
 from src import BoundaryLoop
 from src import CheckpointLoop
 from src import Car
+from src import GA
+from src import WorldData
+from src import EvaluateLoop
+from src import Controller
 
 import pygame as PG
 
-import sys
+import functools
 
 class App:
     def __init__(self):
@@ -24,28 +28,38 @@ class App:
         boundary_map = BoundaryLoop.boundary_loop(self.screen, self.clock)
         map, checkpoints, start_position, start_rotation = CheckpointLoop.checkpoint_loop(self.screen, self.clock, boundary_map)
 
-        cars = Car.Cars(2, start_position, start_rotation)
+        data = WorldData.World(map, checkpoints, start_position, start_rotation)
 
-        surface = PG.surfarray.make_surface(map)
-        while True:
-            self.screen.blit(surface, (0,0))
+        cars = Car.Cars(50, data)
+        # surface = PG.surfarray.make_surface(map)
+        ga = GA.GA(50)
+        evaluate = functools.partial(EvaluateLoop.evaluate_loop, 
+                                     screen=self.screen, clock=self.clock,
+                                     cars=cars,
+                                     controller=Controller.NetController(),
+                                     map=map)
 
-            cars.draw(self.screen)
+        ga.evolutionary_algorithm(evaluate,50)
 
-            for event in PG.event.get():
-                if event.type == PG.QUIT:
-                    sys.exit(0)
+        # EvaluateLoop.evaluate_loop(self.screen, self.clock, cars, pop, Controller.NetController(), map)
 
-            keys = PG.key.get_pressed()  # Checking pressed keys
-            cars.input(0, (keys[PG.K_UP], keys[PG.K_DOWN], keys[PG.K_LEFT], keys[PG.K_RIGHT]))
-            # cars.input(0, (keys[PG.K_UP], keys[PG.K_DOWN], keys[PG.K_LEFT], keys[PG.K_RIGHT]))
+        # while True:
+        #     self.screen.blit(surface, (0,0))
 
-            cars.update(map, checkpoints)
+        #     cars.draw(self.screen)
 
-            PG.display.flip()
-            self.clock.tick(60)
+        #     for event in PG.event.get():
+        #         if event.type == PG.QUIT:
+        #             sys.exit(0)
 
+        #     keys = PG.key.get_pressed()  # Checking pressed keys
+        #     cars.input(0, (keys[PG.K_UP], keys[PG.K_DOWN], keys[PG.K_LEFT], keys[PG.K_RIGHT]))
+        #     # cars.input(0, (keys[PG.K_UP], keys[PG.K_DOWN], keys[PG.K_LEFT], keys[PG.K_RIGHT]))
 
+        #     cars.update(map, checkpoints)
+
+        #     PG.display.flip()
+        #     self.clock.tick(60)
 
 if __name__ == "__main__":
     app = App()
