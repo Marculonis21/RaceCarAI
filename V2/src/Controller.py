5ec847926248ea5d109c5b86d86704b8de904b2a
Marculonis21 <markbecvar@gmail.com>
2023-12-25 13:59:20 +0100

    Net controller done

---
+++ V2/src/Controller.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Controller(ABC):
+    @abstractmethod
+    def action(self,pop, observation) -> np.ndarray:
+        pass
+
+class NetController(Controller):
+
+    def action(self,pop, observation):
+        out = []
+        for ind in range(len(pop)):
+            x = observation[ind]
+            for layer in range(pop[ind].shape[0]):
+                y1 = np.tanh(x@pop[ind,layer])
+                x = y1
+            out.append(x)
+
+        return np.array(out,dtype=object)
