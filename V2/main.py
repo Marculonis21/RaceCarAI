cd2b0607ee855501aa1960c0e2dd1c661932ceed
Marculonis21 <markbecvar@gmail.com>
2023-12-31 16:46:23 +0100

    first test

--- V2/main.py
+++ V2/main.py
@@ -11,9 +11,12 @@ from src import Controller
 from src import AC
 from src import DDPG
 
+import torch
+
 import pygame as PG
 
 import functools
+import numpy as np
 
 import argparse
 import sys
@@ -25,8 +28,10 @@ parser.add_argument("--load", default=None, type=str, help="Load saved track dat
 parser.add_argument("--print_saved", default=False, action="store_true", help="Print all track save names")
 parser.add_argument("--draw", default=False, action="store_true", help="Enable track and car drawing")
 
+REPEATS = 10
+
 class App:
-    def __init__(self):
+    def __init__(self, args):
         PG.init()
         self.screen = PG.display.set_mode((800,800))
         PG.display.set_caption("RACE CAR AI")
@@ -58,23 +63,45 @@ class App:
             print("Save names: ", *names)
             sys.exit()
 
-        # cars = Car.Cars(50, data)
-        # ga = GA.GA(cars.count)
-        # evaluate = functools.partial(EvaluateLoop.evaluate_loop, 
-        #                              screen=self.screen, clock=self.clock,
-        #                              cars=cars,
-        #                              controller=Controller.NetController(),
-        #                              map=data.map)
+        assert data is not None
 
-        # ga.evolutionary_algorithm(evaluate, gen_count=100)
+        # run_data = []
+        # for i in range(REPEATS):
+        #     print(f"GA {i} $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        #     cars = Car.Cars(50, data)
+        #     ga = GA.GA(cars.count)
+        #     evaluate = functools.partial(EvaluateLoop.evaluate_loop, 
+        #                                 screen=self.screen, clock=self.clock,
+        #                                 cars=cars,
+        #                                 controller=Controller.NetController(),
+        #                                 map=data.map)
 
-        # cars = Car.Cars(10, data)
-        # cars = Car.Cars(1, data)
-        # AC.Run(self.screen, self.clock, cars, data.map) 
-        # DDPG.Run(self.screen, self.clock, cars, data.map) 
+        #     fits = ga.evolutionary_algorithm(evaluate, gen_count=100, visualize=args.draw)
+        #     run_data.append(fits)
+
+#         np.save(f"data/run_data/GA_c50_g100.npy", np.array(run_data))
+
+        # run_data = []
+        # for i in range(REPEATS):
+        #     print(f"DDPG {i} $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        #     cars = Car.Cars(1, data)
+        #     fits = DDPG.Run(self.screen, self.clock, cars, data.map, dims1=64, max_runs=1000, visualize=args.draw) 
+        #     run_data.append(fits)
+
+        # np.save(f"data/run_data/DDPG_arch64_64.npy", np.array(run_data))
+
+        run_data = []
+        for i in range(REPEATS):
+            print(f"DDPG {i} $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+            cars = Car.Cars(1, data)
+            fits = DDPG.Run(self.screen, self.clock, cars, data.map, dims1=128, max_runs=1000, visualize=args.draw) 
+            run_data.append(fits)
+
+        np.save(f"data/run_data/DDPG_arch128_64.npy", np.array(run_data))
+            
 
 if __name__ == "__main__":
-    app = App()
     args = parser.parse_args()
+    app = App(args)
     app.Run(args)
     
