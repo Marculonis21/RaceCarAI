f7fc6e23769c6f93261781498c6c2d170f71497b
Marculonis21 <markbecvar@gmail.com>
2024-01-02 13:22:00 +0100

    runs changes

--- V2/src/EvaluateLoop.py
+++ V2/src/EvaluateLoop.py
@@ -8,26 +8,30 @@ import pygame as PG
 import sys
 import numpy as np
 
-def evaluate_loop(screen : PG.Surface, clock : PG.time.Clock, cars : Car.Cars, individuals, controller : Controller.Controller, map : np.ndarray):
+def evaluate_loop(screen : PG.Surface, clock : PG.time.Clock, cars : Car.Cars, individuals, controller : Controller.Controller, map : np.ndarray, visualize):
     surface = PG.surfarray.make_surface(map)
 
     cars.reset()
 
+    obs = np.zeros([cars.count, 6])
     while cars.any_alive():
-        screen.blit(surface, (0,0))
-        cars.draw(screen)
+        if visualize:
+            screen.blit(surface, (0,0))
+            cars.draw(screen)
 
         for event in PG.event.get():
             if event.type == PG.QUIT:
                 sys.exit(0)
 
-        cars.update()
-
-        obs = cars.observation(screen,False)
         actions = controller.action(individuals, obs)
         cars.input_controller(actions)
+        cars.update()
+        next_obs = cars.observation(screen,False)
 
-        PG.display.flip()
-        # clock.tick(60)
+        if visualize:
+            PG.display.flip()
+            # clock.tick(60)
+
+        obs = next_obs
 
     return cars.calc_fitness()
--- V2/src/GA.py
+++ V2/src/GA.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 
-
 import numpy as np
 import random
-
-from src import EvaluateLoop
-
 import copy
+
+class NetController():
+    def action(self,pop, observation):
+        out = []
+        for ind in range(len(pop)):
+            x = observation[ind]
+            for layer in range(pop[ind].shape[0]):
+                y1 = np.tanh(x@pop[ind,layer])
+                x = y1
+            out.append(x)
+
+        return np.array(out,dtype=object)
 
 class GA:
     def __init__(self, pop_size):
         self.POP_SIZE = pop_size
 
         self.NETWORK_ARCHITECTURE = [6,8,4,2]
-        self.CROSSOVER_PROB = 0.75
+        self.CROSSOVER_PROB = 0.85
 
-        self.MUT_PROB = 0.1
-        self.MUT_FLIP_PROB = 0.25
+        self.MUT_PROB= 0.025
 
     def __create_ind(self):
         NN = self.NETWORK_ARCHITECTURE
@@ -28,7 +35,7 @@ class GA:
             w = np.random.uniform(-1, 1, [NN[layer], NN[layer+1]])
             ind.append(w)
 
-        return np.array(ind)
+        return np.array(ind, dtype=object)
 
     def create_population(self, size):
         return np.array([self.__create_ind() for _ in range(size)])
@@ -70,11 +77,10 @@ class GA:
 
     def __mutate(self, p):
         mutant = copy.deepcopy(p)
-        if np.random.random() < self.MUT_PROB:
-            for l in range(mutant.shape[0]):
-                for i in range(mutant[l].ravel().shape[0]):
-                    if np.random.random() < self.MUT_FLIP_PROB:
-                        mutant[l].ravel()[i] = np.random.uniform(-1,1)
+        for l in range(mutant.shape[0]):
+            for i in range(mutant[l].ravel().shape[0]):
+                if np.random.random() < self.MUT_PROB:
+                    mutant[l].ravel()[i] = np.random.uniform(-1,1)
 
         return mutant
 
@@ -83,11 +89,13 @@ class GA:
         return list(map(self.__mutate, pop))
 
     # implements the whole EA
-    def evolutionary_algorithm(self, fitness, gen_count=-1):
+    def evolutionary_algorithm(self, fitness, gen_count=-1, visualize=False):
         pop = self.create_population(self.POP_SIZE)
         G = 0
+        score_history = []
         while G < gen_count or gen_count == -1:
-            fits = fitness(individuals=pop)
+            fits = fitness(individuals=pop, visualize=visualize)
+            score_history.append(fits)
 
             print(G, np.sum(fits)/self.POP_SIZE, np.max(fits)) # prints fitness to console
 
@@ -98,4 +106,5 @@ class GA:
             pop = np.array(pop,dtype=object)
 
             G += 1
-        return pop
+
+        return score_history
